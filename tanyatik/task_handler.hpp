2a760638a6f85c954ff38161183fa277a1862ff2
tanyatik <tanyatik@yandex.ru>
2014-05-05 03:51:10 +0400

    Remove unnecessary comments, change naming

--- tanyatik/task_handler.hpp
+++ tanyatik/task_handler.hpp
@@ -11,24 +11,23 @@ template<typename RequestHandler>
 class TaskHandler {
 private:
     ThreadPool thread_pool_;
-    //TThreadPool thread_pool_;
 
 public:
-    struct Task {
+    struct TaskCreator {
     private:
         std::shared_ptr<TaskHandler> task_handler_;
         int client_id_;
        
     public:
-        Task(std::shared_ptr<TaskHandler> task_handler, int client_id) :
+        TaskCreator(std::shared_ptr<TaskHandler> task_handler, int client_id) :
             task_handler_(task_handler),
             client_id_(client_id)
             {}
      
-        void operator() (Buffer request) {
+        void operator()(Buffer request) {
             task_handler_->addRequestTask(client_id_, request);
         }
-    };
+    }; 
 
     TaskHandler() :
         thread_pool_(1) {}
@@ -39,12 +38,8 @@ public:
         thread_pool_.submit(std::bind(RequestHandler::handleRequest, request));
     }
 /*
-    std::function<void(Buffer)> getInputTaskHandler(int client_id) {
-        std::cerr << "TH: getInputTaskHandler\n";
-        return std::bind(&TaskHandler::addRequestTask, 
-                this, 
-                client_id, 
-                std::placeholders::_1);
+    void putResult(int client_id, Buffer result) {
+        io_server_->push(std::make_pair(client_id, result));
     }
 */
 }; 
