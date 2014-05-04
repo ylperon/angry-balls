798fdd1afce7ceb0041498750466259da29163f6
Oleksandr Driuk <sanya40@ukr.net>
2014-05-04 21:31:14 +0300

    Strategy_Updates

---
+++ permanentstudent/strategy.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <math.h>
+
+#include "basics.h"
+#include "strategy_interface.h"
+
+class Strategy : StrategyInterface {
+
+private:
+    struct BestMoveToCoin
+    {
+        double best_time;
+        Acceleration best_acceleration;
+        int coin_id;
+
+        BestMoveToCoin(const double time, const Acceleration& acceleration, const int coin_index);
+    };
+
+    static const int AngleStepNumber = 10000;
+
+    Player getNextState(const FieldState& state, const Player& previous_state,
+        const Acceleration& acceleration);
+    BestMoveToCoin getBestMove(const FieldState& state, const PlayerId player_id);
+
+public:
+    Acceleration GetTurn(const FieldState& state, const PlayerId player_id);
+};
