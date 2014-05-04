c6711d67d34ea31eced5a7a1e59fc7101bee739b
tanyatik <tanyatik@yandex.ru>
2014-05-05 03:20:52 +0400

    Change logic with output results

--- tanyatik/task_handler.hpp
+++ tanyatik/task_handler.hpp
@@ -1,61 +1,52 @@
 #pragma once
 
 #include <map>
-#include <future>
 
+#include "io_handler.hpp"
 #include "thread_pool.hpp"
 
 namespace tanyatik {
 
 template<typename RequestHandler>
-class ServerTaskHandler {
+class TaskHandler {
 private:
-    std::map<int, std::shared_ptr<std::future<std::vector<char>>>> responds_;
     ThreadPool thread_pool_;
-    std::mutex lock_;
+    //TThreadPool thread_pool_;
 
 public:
-    ServerTaskHandler() {
-        std::cerr << "Create task handler\n";
-    }
-
-    ~ServerTaskHandler() {
-        std::cerr << "Destroy task handler\n";
-    }
-
-    void addRequestTask(int client_id, std::vector<char> request) {
-        std::cerr << "addRequestTask\n";
-        auto future_result = std::make_shared<std::future<std::vector<char>>>
-                (thread_pool_.submit(RequestHandler::getRequestProcessor(request)));
-        std::cerr << "future result\n";
-        lock_.lock();
-        std::cerr << "mutex\n";
-        responds_.insert(std::make_pair(client_id, future_result));
-        lock_.unlock();
-        std::cerr << "end request task\n";
-    }
-
-    std::vector<char> getRespond(int client_id) { 
-        lock_.lock();
-        auto found = responds_.find(client_id);
-        if (found == responds_.end()) {
-            throw std::logic_error("No future respond bound for descriptor");
+    struct Task {
+    private:
+        std::shared_ptr<TaskHandler> task_handler_;
+        int client_id_;
+       
+    public:
+        Task(std::shared_ptr<TaskHandler> task_handler, int client_id) :
+            task_handler_(task_handler),
+            client_id_(client_id)
+            {}
+     
+        void operator() (Buffer request) {
+            task_handler_->addRequestTask(client_id_, request);
         }
-        // block here :(
-        return found->second->get();
-        lock_.unlock();
-    }
+    };
 
-    std::function<void(std::vector<char>)> getInputTaskHandler(int client_id) {
-        return std::bind(&ServerTaskHandler::addRequestTask, 
+    TaskHandler() :
+        thread_pool_(1) {}
+
+    ~TaskHandler() {}
+
+    void addRequestTask(int client_id, Buffer request) {
+        thread_pool_.submit(std::bind(RequestHandler::handleRequest, request));
+    }
+/*
+    std::function<void(Buffer)> getInputTaskHandler(int client_id) {
+        std::cerr << "TH: getInputTaskHandler\n";
+        return std::bind(&TaskHandler::addRequestTask, 
                 this, 
                 client_id, 
                 std::placeholders::_1);
     }
-
-    std::function<std::vector<char>(void)> getOutputTaskHandler(int client_id) {
-        return std::bind(&ServerTaskHandler::getRespond, this, client_id);
-    }
+*/
 }; 
 
 } // namespace tanyatik
