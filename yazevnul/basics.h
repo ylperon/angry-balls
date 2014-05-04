444ab343de3e6b753d5326106a8c8565367cf360
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-05-04 15:58:04 +0400

    Basic datastructures.

---
+++ yazevnul/basics.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+
+struct Point
+{
+    double x;
+    double y;
+};
+
+struct Velocity : Point;
+
+struct Acceleration : Point;
+
+typedef unsigned int PlayerId;
+
+struct Player
+{
+    PlayerId id;
+
+    Point center;
+    double radius;
+
+    Velocity velocity;
+};
+
+typedef int Score;
+
+struct Coin
+{
+    Point center;
+    double radius;
+
+    Score value;
+};
+
+typedef unsigned long long FieldStateId;
+
+struct FieldState
+{
+    FieldStateId id;
+
+    double radius;
+    double ball_radius;
+    double coin_radius;
+    double time_delta;
+    double velocity_max;
+
+    std::vector<Player> players;
+
+    std::vector<Coin> coins;
+};
