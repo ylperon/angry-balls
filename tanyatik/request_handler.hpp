d027d3995c4793c8b1b288a5c7fb96513fc76e1d
tanyatik <tanyatik@yandex.ru>
2014-05-05 18:51:35 +0400

    Add result passing to request handle

--- tanyatik/request_handler.hpp
+++ tanyatik/request_handler.hpp
@@ -2,20 +2,26 @@
 
 namespace tanyatik {
 
-class RequestHandler {
-public:
-    virtual Buffer handleRequest(Buffer request) = 0;
-};
-
 class ProxyRequestHandler : public RequestHandler {
+private:
+    IOTaskHandler *result_handler_;
+
 public:
-    virtual Buffer handleRequest(Buffer request) {
+    ProxyRequestHandler() :
+        result_handler_(nullptr)
+        {}
+   
+    virtual void setResultHandler(IOTaskHandler *result_handler) {
+        result_handler_ = result_handler;
+    }
+
+    virtual void handleRequest(Buffer request, int connection_id) {
         std::cerr << "HANDLE REQUEST\n";
         std::cerr << "REQUEST [" << request.data() << "]\n";
         std::string respond_string("HTTP/1.1 200 OK\n\n<html>hello<html>\n");
         Buffer respond_buffer(respond_string.begin(), respond_string.end()); 
 
-        return respond_buffer;
+        result_handler_->putResult(connection_id, respond_buffer);
     }
 };
 
