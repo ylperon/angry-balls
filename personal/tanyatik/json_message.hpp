ff134f1c8e0b59e3f19e026b46928b8e81f1fdf7
tanyatik <tanyatik@yandex.ru>
2014-05-18 19:34:24 +0400

    Add json message request/respond handler

---
+++ personal/tanyatik/json_message.hpp
@@ -0,0 +1,26 @@
+#include "mio/mio.hpp"
+
+class JsonRequestParser : public mio::RequestParser {
+private:
+    std::weak_ptr<MessageManager> message_manager_;
+
+public:
+    JsonRequestParser(std::weak_ptr<MessageManager> message_manager) :
+        message_manager_(message_manager)
+        {}
+
+    // request is raw json
+    void handleRequest(mio::Buffer request) {
+        auto message = ParseJsonMessage(std::string(request.data()));
+        message_manager_->receiveMessage(message);
+    }
+};
+
+class JsonResponsePrinter {
+public:
+    static mio::Buffer getResponse(const Message& message) {
+        auto json_string = BuildJsonMessage(message);
+
+        return mio::makeBuffer(json_string.begin(), json_string.end());
+    }
+};
