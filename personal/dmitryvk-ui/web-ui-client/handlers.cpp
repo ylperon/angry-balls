c27f95085783888a743ea64d69ecc87590cbb5b2
Dmitry Kalyanov <Kalyanov.Dmitry@gmail.com>
2014-05-19 08:43:44 +0400

    Заглушка для отрисовки поля через html5 canvas

--- personal/dmitryvk-ui/web-ui-client/handlers.cpp
+++ personal/dmitryvk-ui/web-ui-client/handlers.cpp
@@ -6,6 +6,8 @@ using std::string;
 using std::vector;
 using std::ifstream;
 
+#include "index-html-content.cpp"
+
 const HttpResponse index_handler(const HttpRequest& request) {
   HttpResponse result;
   result.version = request.version;
@@ -13,7 +15,7 @@ const HttpResponse index_handler(const HttpRequest& request) {
   result.status_message = "OK";
   result.headers.push_back(HttpHeader("content-type", "text/html; charset=utf-8"));
   
-  string body = "<html><body><h1>Визуализатор: TODO</h1></body></html>";
+  string body = reinterpret_cast<const char*>(index_html_contents);
   result.response_body = vector<unsigned char>(body.begin(), body.end());
   return result;
 }
