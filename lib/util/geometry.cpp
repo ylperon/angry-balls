c80584f3eda4706e40c87f23ead0a12fb1c5e9fb
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-05-19 17:10:10 +0400

    Fix build in geometry.cpp Add build script.

--- lib/util/geometry.cpp
+++ lib/util/geometry.cpp
@@ -14,7 +14,7 @@ double ab::Distance(const ab::Point lhs, const ab::Point rhs)
     return sqrt((lhs.x - rhs.x) * (lhs.x - rhs.x) + (lhs.y - rhs.y) * (lhs.y - rhs.y));
 }
 
-ab::Point Normalize(const Point point)
+ab::Point ab::Normalize(const ab::Point point)
 {
     const double length = Length(point);
     Point result = point;
