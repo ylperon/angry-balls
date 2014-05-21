9bbcb8cbb089ffff80c279275e4dc25cafee0bb2
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-05-21 17:47:18 +0400

    Update emulator interface.

--- include/ab/emulator_interface.h
+++ include/ab/emulator_interface.h
@@ -13,11 +13,11 @@ public:
    /* Physics emulation.
     * FieldState::id of the returned state must be the same as it was in @state.
     */
-   FieldState Emulate(const FieldState& state, const std::vector<Turn>& turns) = 0;
+   virtual FieldState Emulate(const FieldState& state, const std::vector<Turn>& turns) = 0;
 
-   virtual ~EmulatorInterface()
-   {
-   }
+   virtual void SetStepsNumber() = 0;
+
+   virtual ~EmulatorInterface() {}
 };
 
 } // namespace ab
