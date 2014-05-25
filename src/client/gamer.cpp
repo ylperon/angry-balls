261d376ec14c4d05fa7fd3751a13a594d51ce633
Roman Khalkechev <romario@213.180.217.37-red.dhcp.yndx.net>
2014-05-25 16:51:42 +0400

    client updates

--- src/client/gamer.cpp
+++ src/client/gamer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "gamer.h"
+
+#include "client/gamer.h"
 
 namespace ab {
 
@@ -7,7 +8,7 @@ IOClient::IOClient()
 {
     sockfd_ = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd_ < 0) {
-        cerr << "Failed to create socket\n");
+        std::cerr << "Failed to create socket\n";
         throw;
     }
 }
@@ -24,7 +25,7 @@ bool IOClient::Connection(size_t port) const
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     if (connect(sockfd_, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        cerr << "Connection failed\n");
+        std::cerr << "Connection failed\n";
         return false;
     }
     return true;
@@ -69,12 +70,6 @@ int IOClient::RecvAll(std::string& buf, int flags) const
 }
 
 template <class Strategy>
-Gamer<Strategy>::Gamer() {}
-
-template <class Strategy>
-Gamer<Strategy>::~Gamer() {}
-
-template <class Strategy>
 bool Gamer<Strategy>::ConnectionToServer(size_t port)
 {
     while (!client_.Connection(port)) {
@@ -84,54 +79,57 @@ bool Gamer<Strategy>::ConnectionToServer(size_t port)
     ClientSubscribeRequestMessage client_subscribe_request_message;
     int send_result = client_.SendAll(BuildJsonMessage(&client_subscribe_request_message), 0);
     if (send_result == -1) {
-        cerr << "SendAll is failed\n");
+        std::cerr << "SendAll(client_subscribe_request_message, 0) is failed\n";
         return false;
     }
 
     std::string json_start_game_answer;
     if (client_.RecvAll(json_start_game_answer, 0) == -1) {
-        cerr << "RecvAll is failed\n");
+        std::cerr << "RecvAll(json_start_game_answer, 0) is failed\n";
         return false;
     }
 
-    std::unique_ptr<Message> result_message = ParseJsonMessage(json_start_game_answer)
-    if (!result_message) {
-        cerr << "Unsuccessful parse\n");
+    std::unique_ptr<Message> message = ParseJsonMessage(json_start_game_answer);
+    if (!message) {
+        std::cerr << "Unsuccessful message parse\n";
         return false;
     }
 
-    if (result_message->type != MessageType::kClientSubscribeResultMessage) {
-        cerr << "Bad response type: " + ToString(result_message->type)) + '\n';
+    if (message->type != MessageType::kClientSubscribeResultMessage) {
+        std::cerr << "Bad response type: " + ToString(message->type) + '\n';
         return false;
     }
 
-    unique_ptr<ClientSubscribeResultMessage> client_subscribe_result_message
-            (dynamic_cast<const ClientSubscribeResultMessage* const>(result_message.release()));
+    std::unique_ptr<ClientSubscribeResultMessage> client_subscribe_result_message
+                (dynamic_cast<ClientSubscribeResultMessage*>(message.release()));
 
     if (!client_subscribe_result_message || !client_subscribe_result_message->result) {
-        cerr << "Server refused to accept client\n");
+        std::cerr << "Server refused to accept client\n";
         return false;
     }
 
     id_ = client_subscribe_result_message->player_id;
-    cerr << "Connected to game server as client with id = " << player_id << endl;
+    std::cerr << "Connected to game server as client with id = " << id_ << std::endl;
     return true;
 }
 
 template <class Strategy>
-void Gamer<Strategy>::Game() const
+void Gamer<Strategy>::Game(size_t port) const
 {
+    if (!ConnectionToServer(port))
+        return;
+
     std::string json_state;
     while (client_.RecvAll(json_state, 0) != -1) {
         std::string json_turn;
-        if (Turn(json_state, &json_turn)
+        if (Turn(json_state, &json_turn)) {
             int send_result = client_.SendAll(json_turn, 0);
             if (send_result == -1) {
-                cerr << "SendAll(json_turn) is failed\n");
-                return;
+                std::cerr << "SendAll(json_turn) is failed\n";
+                continue;
             }
         } else {
-            cerr << "Turn is failed\n";
+            std::cerr << "Turn(json_state, &json_turn) is failed\n";
         }
     }
 }
@@ -139,24 +137,24 @@ void Gamer<Strategy>::Game() const
 template <class Strategy>
 bool Gamer<Strategy>::Turn(const std::string& json_state, std::string* json_turn) const
 {
-    unique_ptr<Message> message = ParseJsonMessage(json_state);
+    std::unique_ptr<Message> message = ParseJsonMessage(json_state);
     if (!message) {
-        cerr << "Unsuccessful parse\n");
+        std::cerr << "Unsuccessful message parse\n";
         return false;
     }
     if (message->type != MessageType::kFieldStateMessage) {
-        cerr << "Bad message type: " + ToString(message->type) + '\n';
+        std::cerr << "Bad message type: " + ToString(message->type) + '\n';
         return false;
     }
     
-    unique_ptr<FieldStateMessage> field_state_message
-                                            (dynamic_cast<FieldStateMessage*>(messsage.release()));
+    std::unique_ptr<FieldStateMessage> field_state_message
+                                            (dynamic_cast<FieldStateMessage*>(message.release()));
 
     TurnMessage turn_message;
-    turn_message.Turn.player_id = id_;
-    turn_message.Turn.state_id = field_state_message->id;
-    turn_message.Turn.acceleration = strategy_(field_state_message->field_state, id_);
-    turn_message.FieldStateId = field_state_message->field_state.id_;
+    turn_message.turn.player_id = id_;
+    turn_message.turn.state_id = field_state_message->field_state.id;
+    turn_message.turn.acceleration = strategy_(field_state_message->field_state, id_);
+    turn_message.state_id = field_state_message->field_state.id;
 
     *json_turn = BuildJsonMessage(&turn_message);
     return true;
