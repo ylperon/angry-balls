ed6d74b2b180d067181511206ecf51d36bc0cd3f
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-06-15 03:07:40 +0400

    Fix style.

--- src/web-ui-client/thread-pool.cpp
+++ src/web-ui-client/thread-pool.cpp
@@ -1,96 +1,75 @@
-#include <iostream>
-#include <stdexcept>
-#include <cstring>
-#include <sstream>
-#include <memory>
-#include <functional>
-#include <condition_variable>
-#include <chrono>
-#include <string>
-
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-
 #include "webserver.hpp"
 
-using std::string;
-using std::runtime_error;
-using std::strerror;
-using std::cerr;
-using std::endl;
-using std::shared_ptr;
-using std::vector;
-using std::ostringstream;
-using std::unique_lock;
-using std::mutex;
-using std::function;
-using std::thread;
+#include <iostream>
+#include <stdexcept>
+#include <string>
 
-ThreadPool::ThreadPool(unsigned num_workers) {
-  {
-    // Should use lock to ensure memory fence before workers have started
-    unique_lock<mutex> lock(data_mutex);
-    should_quit = false;
-  }
-  function<void()> worker_thread_fn = [=]() { this->worker_func(); };
-  workers.reserve(num_workers);
-  for (unsigned worker_iter = 0; worker_iter < num_workers; ++worker_iter) {
-    workers.emplace_back(worker_thread_fn);
-  }
+ThreadPool::ThreadPool(unsigned num_workers)
+{
+    {
+        // Should use lock to ensure memory fence before workers have started
+        std::unique_lock<std::mutex> lock(data_mutex);
+        should_quit = false;
+    }
+    std::function<void()> worker_thread_fn = [this]() { this->worker_func(); };
+    workers.reserve(num_workers);
+    for (unsigned worker_iter = 0; worker_iter < num_workers; ++worker_iter)
+        workers.emplace_back(worker_thread_fn);
 }
 
-ThreadPool::~ThreadPool() {
+ThreadPool::~ThreadPool()
+{
   shutdown();
 }
 
-void ThreadPool::enqueue(WorkerFunction fn) {
-  {
-    unique_lock<mutex> lock(data_mutex);
-    work_queue.push(fn);
-  }
-  has_more_work_cv.notify_one();
-}
-
-bool ThreadPool::worker_dequeue(WorkerFunction& result) {
-  unique_lock<mutex> lock(data_mutex);
-  while (!should_quit && work_queue.empty()) {
-    has_more_work_cv.wait(lock);
-  }
-  if (should_quit) {
-    return false;
-  } else {
-    result = work_queue.front();
-    work_queue.pop();
-    return true;
-  }
-}
-
-void ThreadPool::worker_func() {
-  while (true) {
-    try {
-      WorkerFunction fn;
-      if (!worker_dequeue(fn)) {
-        return;
-      }
-      fn();
-    } catch (...) {
-      cerr << "Caught exception in worker function" << endl;
+void ThreadPool::enqueue(WorkerFunction fn)
+{
+    {
+        std::unique_lock<std::mutex> lock(data_mutex);
+        work_queue.push(fn);
     }
-  }
+    has_more_work_cv.notify_one();
 }
 
-void ThreadPool::shutdown() {
-  {
-    unique_lock<mutex> lock(data_mutex);
-    should_quit = true;
-  }
-  has_more_work_cv.notify_all();
-  for (thread& thread: workers) {
-    thread.join();
-  }
-  workers.clear();
+bool ThreadPool::worker_dequeue(WorkerFunction& result)
+{
+    std::unique_lock<std::mutex> lock(data_mutex);
+    while (!should_quit && work_queue.empty())
+        has_more_work_cv.wait(lock);
+
+    if (should_quit) {
+        return false;
+    } else {
+        result = work_queue.front();
+        work_queue.pop();
+        return true;
+    }
 }
 
+void ThreadPool::worker_func()
+{
+    while (true) {
+        try {
+            WorkerFunction fn;
+            if (!worker_dequeue(fn))
+                return;
+
+            fn();
+        } catch (...) {
+            std::cerr << "Caught exception in worker function" << std::endl;
+        }
+    }
+}
+
+void ThreadPool::shutdown()
+{
+    {
+        std::unique_lock<std::mutex> lock(data_mutex);
+        should_quit = true;
+    }
+    has_more_work_cv.notify_all();
+    for (std::thread& thread: workers)
+        thread.join();
+
+    workers.clear();
+}
