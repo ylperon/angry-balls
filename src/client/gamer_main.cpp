438f6462dab7f3612b5c0a93056a4ace1cb65166
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-07-09 01:32:52 +0400

    Refactor gamer options.

--- src/client/gamer_main.cpp
+++ src/client/gamer_main.cpp
@@ -1,55 +1,80 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
-#include "client/gamer.h"
-#include "strategies/strategies.h"
+#include <util/cast.h>
 
-enum class StrategyType
+#include <client/gamer.h>
+#include <strategies/strategies.h>
+#include <strategies/factory.h>
+
+std::string StrategyNamesSeparatedByComma()
 {
-    DoNothin,
-    MoveToClosest,
-    Predictive,
-    Drunk,
-    Buffalo,
-    RandomAcceleration
-};
+    std::string result;
+    for (const auto& name: ab::GetAllStrategyNamesStr()) {
+        if (!result.empty())
+            result += ',';
+        result += name;
+    }
+
+    return result;
+}
+
+std::string GetUsageMessage(const std::string& binary_name)
+{
+    const std::string usage_message
+        = std::string{"Usage: "} + binary_name + " " + "[OPTIONS]" + "\n\n"
+          + "Required arguments:\n"
+          + "  {-p|--port}   Server port.\n"
+          + "  --strategy    Strategy to use. Must be in: {"
+              + StrategyNamesSeparatedByComma() + "}"
+          + "\n\n"
+          + "Optional arguments:\n"
+          + "  {-?|--help}   Print usage."
+          + "\n";
+
+    return usage_message;
+}
+
+void Validate(const std::vector<std::string>& argvv)
+{
+    if (2 == argvv.size() && (argvv[1] == "-?" || argvv[1] == "--help")) {
+        std::cerr << GetUsageMessage(argvv.front());
+        std::exit(0);
+    }
+
+    if (5 != argvv.size() ||
+        !(argvv.end() != std::find(argvv.begin(), argvv.end(), "-p") ^
+            argvv.end() != std::find(argvv.begin(), argvv.end(), "--port")) ||
+        argvv.end() == std::find(argvv.begin(), argvv.end(), "--strategy") ||
+        argvv.end() != std::find(argvv.begin(), argvv.end(), "-?") ||
+        argvv.end() != std::find(argvv.begin(), argvv.end(), "--help")
+       )
+    {
+        std::cerr << "Wrong arguments." << "\n";
+        std::cerr << GetUsageMessage(argvv.front());
+        std::exit(0);
+    }
+}
 
 struct Options
 {
     size_t port;
-    StrategyType strategy;
+    ab::StrategyName strategy;
 };
 
 Options ParseOptions(int argc, char** argv)
 {
-    const std::string usage_message
-        = std::string(argv[0]) + " --port <port> --strategy <strategy>\n"
-          + "Where <strategy> is in {do-nothing, move-to-closest, predictive, drunk, buffalo"
-          + ", random}";
-
-    if (5 != argc || std::string("--port") != argv[1] || std::string("--strategy") != argv[3]) {
-        std::cerr << usage_message << std::endl;
-        std::exit(1);
-    }
+    const std::vector<std::string> argvv(argv, argv + argc);
+    Validate(argvv);
 
     Options options;
-    options.port = atoi(argv[2]);
-
-    if (std::string("do-nothing") == argv[4])
-        options.strategy = StrategyType::DoNothin;
-    else if (std::string("move-to-closest") == argv[4])
-        options.strategy = StrategyType::MoveToClosest;
-    else if (std::string("predictive") == argv[4])
-        options.strategy = StrategyType::Predictive;
-    else if (std::string("drunk") == argv[4])
-        options.strategy = StrategyType::Drunk;
-    else if (std::string("buffalo") == argv[4])
-        options.strategy = StrategyType::Buffalo;
-    else if (std::string("random") == argv[4])
-        options.strategy = StrategyType::RandomAcceleration;
-    else {
-        std::cerr << usage_message << std::endl;
-        std::exit(1);
+    for (size_t index = 1; index < argvv.size(); ++index) {
+        const std::string& arg = argvv[index];
+        if (arg == "-p" || arg == "--port")
+            options.port = std::atoi(argvv.at(index + 1).c_str());
+        else if (arg == "--strategy")
+            options.strategy = ab::FromString<ab::StrategyName>(argvv.at(index + 1));
     }
 
     return options;
@@ -58,35 +83,10 @@ Options ParseOptions(int argc, char** argv)
 int main(int argc, char** argv)
 {
     const Options options = ParseOptions(argc, argv);
+    ab::StrategiesFactory factory;
     ab::Gamer gamer;
     gamer.SetPort(options.port);
-    switch (options.strategy) {
-        case StrategyType::DoNothin: {
-            gamer.SetStrategy(std::unique_ptr<ab::StrategyInterface>(
-                        new ab::DoNothingStrategy()));
-            break;
-        } case StrategyType::MoveToClosest: {
-            gamer.SetStrategy(std::unique_ptr<ab::StrategyInterface>(
-                        new ab::MoveToClosestStrategy()));
-            break;
-        } case StrategyType::Predictive: {
-            gamer.SetStrategy(std::unique_ptr<ab::StrategyInterface>(
-                        new ab::PredictiveStrategy()));
-            break;
-        } case StrategyType::Drunk: {
-            gamer.SetStrategy(std::unique_ptr<ab::StrategyInterface>(
-                        new ab::DrunkStrategy()));
-            break;
-        } case StrategyType::Buffalo: {
-            gamer.SetStrategy(std::unique_ptr<ab::StrategyInterface>(
-                        new ab::BuffaloStrategy()));
-            break;
-        } case StrategyType::RandomAcceleration: {
-            gamer.SetStrategy(std::unique_ptr<ab::StrategyInterface>(
-                        new ab::RandomAccelerationStrategy()));
-            break;
-        }
-    }
+    gamer.SetStrategy(factory.Create(options.strategy));
     gamer.Run();
 
     return 0;
